722473e3b9d598087270ed4fa2dfef57a9b7a098
AlexandraKrause <you@example.com>
2022-05-10 21:25:00 +0200

    check literature duplicates

---
+++ checkLiteratur.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(readxl)
+
+checkLiteratureTemp <- read_excel("methods-excel.xlsx")
+
+checkLiterature <- checkLiteratureTemp[!is.na(checkLiteratureTemp$Authors),]
+doubledLiterature <- checkLiterature[duplicated(checkLiterature$Authors),]
